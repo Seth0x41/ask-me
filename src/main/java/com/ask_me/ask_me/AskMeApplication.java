0a419601a3c1530343e98609485c77f27814e562
seth0x41 <mahmoudalarby41@gmail.com>
2024-12-30 12:19:10 +0200

    init

---
+++ src/main/java/com/ask_me/ask_me/AskMeApplication.java
@@ -0,0 +1,18 @@
+package com.ask_me.ask_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskMeApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+
+
+
+}
---
+++ src/test/java/com/ask_me/ask_me/AskMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask_me.ask_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
